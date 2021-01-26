ebd93b39fda1c5bdda2c4a44c1c6e641142909b6
alex <alex@tekartik.com>
2021-01-26 11:15:52 +0100

    fix: lint and and github actions

--- bluetooth_test_app/lib/main.dart
+++ bluetooth_test_app/lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           }();
         },
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
