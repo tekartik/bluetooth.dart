7c57cb2e525a3b1136c3f70f3fc0c50acc1bd42b
Alexandre Roux <alex@tekartik.com>
2024-03-25 11:19:31 +0100

    fix lints

--- bluetooth_test_app/tool/test_app_build_menu.dart
+++ bluetooth_test_app/tool/test_app_build_menu.dart
@@ -2,5 +2,5 @@ import 'package:tekartik_build_menu_flutter/app_build_menu.dart';
 
 var appPath = '.';
 Future main(List<String> arguments) async {
-  mainMenu(arguments, menuAppContent);
+  mainMenuConsole(arguments, menuAppContent);
 }
